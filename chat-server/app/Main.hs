3af7a1347e2b251adceca49acd0fcbbc5ea48271
ponta027 <ponta027@gmail.com>
2019-01-08 01:06:16 +0900

    modify forkIO

--- chat-server/app/Main.hs
+++ chat-server/app/Main.hs
@@ -3,6 +3,12 @@ module Main where
 import Lib
 import Network.Socket
 import System.IO
+import Control.Concurrent
+import Control.Monad.Fix( fix )
+
+
+type Msg = String
+
 
 main :: IO ()
 main = do
@@ -10,14 +16,16 @@ main = do
     setSocketOption sock ReuseAddr 1 
     bind sock (SockAddrInet 4242 iNADDR_ANY)
     listen sock 2
-    mainLoop sock
+    chan <- newChan
+    mainLoop sock   chan
 
+{--
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
     conn <- accept sock     -- accept a connection and handle it
     runConn conn            -- run our server's logic
     mainLoop sock           -- repeat
-
+--}
 {--
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
@@ -25,9 +33,27 @@ runConn (sock, _) = do
     close sock
 --}
 
-runConn::(Socket,SockAddr) -> IO()
-runConn (sock,_) = do
+runConn::(Socket,SockAddr) -> Chan Msg -> IO()
+runConn (sock,_) chan = do
+    let broadcast msg = writeChan chan msg  -- broadcast msg 
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
-    hPutStrLn hdl "Hello"
-    hClose hdl
+    commLine <- dupChan chan                -- commLine Channel 
+    --https://qiita.com/eielh/items/3b793dbedcc6c9dc125c
+    forkIO $ fix $ \loop -> do              -- forkIO ::IO() -> IO ThreadId
+        line <- readChan commLine           -- readChan commLine::Chan
+        putStrLn $ "Msg:" ++ show line
+        hPutStrLn hdl line                  -- response 
+        loop                                -- call loop
+    fix $ \loop -> do
+        line <- fmap init (hGetLine hdl)    -- hGetLine::Handle->IO String
+        broadcast line                      -- call broadcast msg
+        loop                                    
+
+
+mainLoop :: Socket -> Chan Msg -> IO ()
+mainLoop sock chan = do
+    conn <- accept sock
+    forkIO (runConn conn chan)
+    mainLoop sock chan
+
